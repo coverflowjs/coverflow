deb7f67b6100d2278980bf94337f347b2f09a94c
basti <info@dynpages.de>
2012-08-26 09:14:16 +0200

    refactored internals:
    options related:
    	- add itemscale to options (and take care for negative margins then)
    	- multiple trigger support, defaults to focus and click
    	- removed center and recenter options (don't see a usecase here)
    	- add animation related options: duration + easing
    
    jQuery 1.8 related:
    	- drop prefix testing
    
    jQueryUI 1.9:
    	- add jQueryUI 1.9 bindings
    	- add destroy method (needs testing)
    	- allow orientation change by providing _init() (needs testing + custom event)
    
    internals:
    	- use easier assignments (dropped props-Property)
    	- whitespacing - see jQueryUI code guidelines
    	- add: beforeselect event
    	- fix: move select event trigger - triggers after animation finished
    	- fix: animation stop - jump to end
    	- fix: currentIndex assignMent
    	- refactored internals to make the code DRY

--- src/js/ui.coverflow.js
+++ src/js/ui.coverflow.js
@@ -1,188 +1,280 @@
 /*
- * jQuery UI CoverFlow
-   Re-written for jQueryUI 1.8.6/jQuery core 1.4.4+ by Addy Osmani with adjustments
-   Maintenance updates for 1.8.9/jQuery core 1.5, 1.6.2 made.
-
-   Original Component: Paul Bakaus for jQueryUI 1.7 
+ * jQuery UI CoverFlow II
+ *
+ * Updates for jQuery 1.8 / jQueryUI 1.9 Sebastian Sauer
+ *
+ * Re-written for jQueryUI 1.8.6/jQuery core 1.4.4+ by Addy Osmani with adjustments
+ * Maintenance updates for 1.8.9/jQuery core 1.5, 1.6.2 made.
+ *
+ * Original Component: Paul Bakaus for jQueryUI 1.7
+ *
+ * Released under the MIT license.
+ *
+ * Depends:
+ *  jquery.ui.core.js
+ *  jquery.ui.widget.js
+ *  jquery.ui.effect.js
+ *
+ * Events:
+ *  beforeselect
+ *  select
  */
-(function($){
+(function ( $ ) {
 
-	function getPrefix( prop ){  
-	        var prefixes = ['Moz','Webkit','Khtml','O','ms'],  
-	            elem     = document.createElement('div'),  
-	            upper      = prop.charAt(0).toUpperCase() + prop.slice(1),  
-	            pref     = "",
-				len 	 = 0;
-	
-	        for(len = prefixes.length; len--;){  
-	            if((prefixes[len] + upper) in elem.style){  
-	                pref = (prefixes[len]);  
-	            }  
-	        }  
-	
-	        if(prop in elem.style){  
-	            pref = (prop);  
-	        }  
-			return pref;
-	  }
-	
-
-	var vendorPrefix = getPrefix('transform');
-	
-	$.easing.easeOutQuint = function (x, t, b, c, d) {
-		return c*((t=t/d-1)*t*t*t*t + 1) + b;
+	var _capitalize = function( str ) {
+		return str.charAt( 0 ).toUpperCase() + str.slice( 1 );
 	};
 
-	$.widget("ui.coverflow", {
-	
-	   options: {
-	        items: "> *",
-			orientation: 'horizontal',
-			item: 0,
-			trigger: 'click',
-			center: true, //If false, element's base position isn't touched in any way
-			recenter: true //If false, the parent element's position doesn't get animated while items change
-			
-	  },
-		
-		_create: function() {
-			
-			var self = this, o = this.options;
-			this.items = $(o.items, this.element);
-			this.props = o.orientation == 'vertical' ? ['height', 'Height', 'top', 'Top'] : ['width', 'Width', 'left', 'Left'];
-			//For < 1.8.2: this.items['outer'+this.props[1]](1);
-			
-			this.itemSize = 0.73 * this.items.innerWidth();
+	$.widget( "ui.coverflow", {
+
+		options: {
+			items: "> *",
+			// scale left/right images - 0>x<1
+			itemSize : .73,
+			orientation: "horizontal",
+			active: 0,
+			duration : 200,
+			easing: "easeOutQuint",
+			// selection triggers
+			trigger: [
+				"focus",
+				"click"
+			]
+		},
+
+		_create: function () {
+
+			var o = this.options,
+				itemBindings = {},
+				i, binding;
+
+			this.items = this.element.find( o.items );
+
+			this.duration = o.duration;
+			this.currentIndex = o.active;
+
+			if( $.isArray( o.trigger ) ) {
+				// TODO: jshint doesn't like the assignment
+				for( i = 0; binding = o.trigger[ i++ ]; ) {
+					itemBindings[ binding ] = this._select;
+				}
+
+			} else if( ( typeof o.trigger === "string" ) && $.trim( o.trigger ) ) {
+
+				itemBindings[ $.trim( o.trigger ).toLowerCase() ] = this._select;
+
+			} else {
+
+				itemBindings.click = this._select;
+
+			}
+
+			this.origElementDimensions = {
+				width: this.element.width(),
+				height: this.element.height()
+			}
+			this._on( this.items, itemBindings );
+
+		},
+		_init : function () {
+
+			var o = this.options,
+				css;
+
+			if( o.orientation === "vertical" ) {
+				this._topOrLeft = "top";
+				this._widthOrHeight = "height";
+			} else {
+				this._topOrLeft = "left";
+				this._widthOrHeight = "width";
+			}
+
+			o.itemSize = parseFloat( o.itemSize );
+			o.itemSize = o.itemSize < 1 && o.itemSize > 0
+				? o.itemSize
+				: .73;
+
+			this.itemSize = o.itemSize * this.items.innerWidth();
+
+			// apply a negative margin so items stack
+			this.items.css({
+				margin : "" + -1 * Math.floor( o.itemSize / 2 * this.items.innerWidth() ) + "px"
+			});
+
 			this.itemWidth = this.items.width();
 			this.itemHeight = this.items.height();
-			this.duration = o.duration;
-			this.current = o.item; //initial item
-			
 
-			//Bind click events on individual items
-			this.items.bind(o.trigger, function() {
-				self.select(this);
-				
-			});
+			this.outerWidthOrHeight = ( this._widthOrHeight === "width" )
+				? this.element.parent().outerWidth( false )
+				: this.element.parent().outerHeight( false );
 
+			//Center the actual parent"s left side within it's parent
+			css = this._getCenterPosition();
+			// make sure there's enough space
+			css[ this._widthOrHeight ] = this.itemWidth * this.items.length;
 
-			//Center the actual parent's left side within it's parent
-
-			this.element.css(this.props[2],
-				(o.recenter ? -this.current * this.itemSize/2 : 0)
-				+ (o.center ? this.element.parent()[0]['offset'+this.props[1]]/2 - this.itemSize/2 : 0) //Center the items container
-				- (o.center ? parseInt(this.element.css('padding'+this.props[3]),10) || 0 : 0) //Subtract the padding of the items container
-			);
+			this.element
+				.addClass( "ui-coverflow" )
+				.css( css )
+				.parent()
+				.addClass( "ui-coverflow-wrapper" );
 
 			//Jump to the first item
-			this._refresh(1, 0, this.current);
+			this._refresh( 1, 0, this.currentIndex );
+		},
+		_getCenterPosition : function () {
+			var animation = {},
+				o = this.options;
+
+			animation[ this._topOrLeft ] = Math.floor(
+				- this.currentIndex * this.itemSize / 2
+				//Center the items container
+				+ this.outerWidthOrHeight / 2 - this.itemSize / 2
+				//Subtract the padding of the items container
+				- parseInt( this.element.css( "padding" + _capitalize( this._topOrLeft ) ), 10 ) || 0
+			);
+			return animation;
+		},
+		_select: function ( ev ) {
+
+			this.select( ev.currentTarget );
 
 		},
-		
-		select: function(item, noPropagation) {
-		
-			
-			this.previous = this.current;
-			this.current = !isNaN(parseInt(item,10)) ? parseInt(item,10) : this.items.index(item);
-			
+		select : function( item ) {
 
-			//Don't animate when clicking on the same item
-			if(this.previous == this.current) return false; 
-			
+			var o = this.options,
+				index = ! isNaN( parseInt( item, 10 ) )
+					? parseInt( item, 10 )
+					: this.items.index( item );
+
+			if( this.currentIndex === index ) {
+				return false;
+			}
+
+			if( false === this._trigger(
+					"beforeselect",
+					null,
+					this._ui(
+						this.items.index( index ), index
+					)
+				)
+			) {
+				return false;
+			}
+
+			this.previousIndex = this.currentIndex;
+
+			this.currentIndex = index;
+
+			var self = this,
+				to = Math.abs( self.previous - self.currentIndex ) <= 1
+					? self.previousIndex
+					: self.currentIndex + ( self.previousIndex < self.currentIndex ? -1 : 1 ),
+				animation = {
+					coverflow : 1
+				};
+
 			//Overwrite $.fx.step.coverflow everytime again with custom scoped values for this specific animation
-			var self = this, to = Math.abs(self.previous-self.current) <=1 ? self.previous : self.current+(self.previous < self.current ? -1 : 1);
-			$.fx.step.coverflow = function(fx) { self._refresh(fx.now, to, self.current); };
-			
+			$.fx.step.coverflow = function( fx ) {
+				self._refresh( fx.now, to, self.currentIndex );
+			};
+
 			// 1. Stop the previous animation
-			// 2. Animate the parent's left/top property so the current item is in the center
+			// 2. Animate the parent"s left/top property so the current item is in the center
 			// 3. Use our custom coverflow animation which animates the item
 
-			var animation = { coverflow: 1 };
-		
-		
-			animation[this.props[2]] = (
-				(this.options.recenter ? -this.current * this.itemSize/2 : 0)
-				+ (this.options.center ? this.element.parent()[0]['offset'+this.props[1]]/2 - this.itemSize/2 : 0) //Center the items container
-				- (this.options.center ? parseInt(this.element.css('padding'+this.props[3]),10) || 0 : 0) //Subtract the padding of the items container
-			);
-		
-		
-			
-			//Trigger the 'select' event/callback
-			if(!noPropagation) this._trigger('select', null, this._uiHash());
-			
-			this.element.stop().animate(animation, {
-				duration: this.options.duration,
-				easing: 'easeOutQuint'
-			});
-			
+			$.extend( animation, this._getCenterPosition() );
+
+			this.element
+				// jump to end and release select trigger
+				.stop( true, true )
+				.animate(
+					animation,
+					{
+						duration: this.options.duration,
+						easing: o.easing
+					},
+					function () {
+						// fire select after animation has finished
+						self._trigger( "select", null, this._ui() );
+					}
+				);
+
+			return true;
 		},
-		
-		_refresh: function(state,from,to) {
-		
-	
-			var self = this, offset = null;
-	
-			
-			this.items.each(function(i) {
-			
-				
-				var side = (i == to && from-to < 0 ) ||  i-to > 0 ? 'left' : 'right',
-					mod = i == to ? (1-state) : ( i == from ? state : 1 ),
-					before = (i > from && i != to),
-					css = { zIndex: self.items.length + (side == "left" ? to-i : i-to) };
-				    //css[($.browser.safari ? 'webkit' : ($.browser.opera ? 'O' : 'Moz'))+'Transform'] = 'matrix(1,'+(mod * (side == 'right' ? -0.2 : 0.2))+',0,1,0,0) scale('+(1+((1-mod)*0.3)) + ')'; 
-	           
-	
-				if(vendorPrefix == 'ms' || vendorPrefix == ""){
-		
-					css["filter"] = "progid:DXImageTransform.Microsoft.Matrix(sizingMethod='auto expand', M11=1, M12=0, M21=" + (mod * (side == 'right' ? -0.2 : 0.2)) + ", M22=1";
-					css[self.props[2]] = ( (-i * (self.itemSize/2)) + (side == 'right'? -self.itemSize/2 : self.itemSize/2) * mod );
-		
-					if(i == self.current){
-					css.width = self.itemWidth * (1+((1-mod)*0.3));
-					css.height = css.width * (self.itemHeight / self.itemWidth);
-					css.top = -((css.height - self.itemHeight) / 3);
 
-					css.left -= self.itemWidth/6 -50;
-					}
-					else{
-					css.width = self.itemWidth;
-					css.height = self.itemHeight;
-					css.top = 0;
+		_refresh: function( state, from, to ) {
 
-					if(side == "left"){
-					css.left -= self.itemWidth/5 -50;
-					}
-		
-					}//end if
-		
-		
-				}else{
-	
-					css[vendorPrefix + 'Transform'] = 'matrix(1,'+(mod * (side == 'right' ? -0.2 : 0.2))+',0,1,0,0) scale('+(1+((1-mod)*0.3)) + ')'; 
-					css[self.props[2]] = ( (-i * (self.itemSize/2)) + (side == 'right'? -self.itemSize/2 : self.itemSize/2) * mod );
-		
-				}
-	
+			var self = this,
+				offset = null;
 
-				$(this).css(css);
+			this.items.each( function ( i ) {
 
+				var side = ( ( i === to && from - to < 0 ) || i - to  > 0 )
+						? "left"
+						: "right",
+					mod = ( i === to )
+						? ( 1 - state )
+						: ( i === from ? state : 1 ),
+					before = ( i > from && i !== to ),
+					css = {
+						zIndex: self.items.length + ( side === "left" ? to - i : i - to )
+					},
+					scale = ( 1 + ( ( 1 - mod ) * 0.3 ) ),
+					matrixT;
+
+				css[ self._topOrLeft ] = (
+					( -i * ( self.itemSize / 2 ) )
+					+ ( side === "right"
+						? -self.itemSize / 2
+						: self.itemSize / 2
+					) * mod
+				);
+
+				// transponed matrix
+				matrixT = [
+					scale, ( mod * ( side === "right" ? -0.2 : 0.2 ) ),
+					0, scale,
+					0, 0
+				];
+
+				css.transform = "matrix(" + matrixT.join( "," ) + ")";
+
+				$( this ).css( css );
 
 			});
 
-			this.element.parent().scrollTop(0);
-			
+			this.element
+				.parent()
+				.scrollTop( 0 );
 		},
-		
-		_uiHash: function() {
+
+		_ui : function ( active, index ) {
 			return {
-				item: this.items[this.current],
-				value: this.current
+				active: active || this.items.index( this.currentIndex ),
+				index: index || this.currentIndex
 			};
+		},
+		_destroy : function () {
+
+			this.element
+				.css( this.origElementDimensions )
+				.removeClass( 'ui-coverflow' )
+				.parent()
+				.removeClass( 'ui-coverflow-wrapper' );
+
+			this.items.each( function () {
+				// TODO: needs testing
+				// remove transform
+				this.style = this.style.replace( /(-)?(webkit|moz|o|ms)?transform.*;/i, '' );
+				// remove margin
+				this.style = this.style.replace( /margin.*;/, '' );
+			});
+
+			this._superApply( "destroy", arguments );
 		}
-		
+
 	});
 
-	
-})(jQuery); 
+
+})( jQuery );
