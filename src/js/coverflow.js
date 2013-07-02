716f988142f8e8f40f64721b9ae71459af305987
Brandon Belvin <brandon@belvinfamily.com>
2013-07-01 20:52:52 -0500

    Update requestAnimationFrame.js

--- src/js/coverflow.js
+++ src/js/coverflow.js
@@ -25,11 +25,15 @@
 (function( $, document, window ) {
 	"use strict";
 	
-	// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-	// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-
-	// requestAnimationFrame polyfill by Erik Möller
-	// fixes from Paul Irish and Tino Zijdel
+	/**
+	 * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+	 * http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+	 * 
+	 * requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+	 * MIT license
+	 * 
+	 * @see https://gist.github.com/paulirish/1579671
+	 */
 
 	var el = document.body || document.documentElement,
 		style = el.style,
@@ -40,31 +44,31 @@
 		capitalize = function( string ) {
 			return string.charAt( 0 ).toUpperCase() + string.slice( 1 );
 		};
-
-	for( ; x < vendorsLength && ! window.requestAnimationFrame; x++ ) {
-		requestAnimationFrame = window[ vendors[ x ] + "RequestAnimationFrame" ];
-		cancelAnimationFrame =
-			window[ vendors[ x ] + "CancelAnimationFrame" ]
-			|| window[ vendors[ x ] + "CancelRequestAnimationFrame" ];
+	
+	for( ; x < vendorsLength && ! window.requestAnimationFrame; ++x ) {
+		window.requestAnimationFrame = window[ vendors[ x ] + "RequestAnimationFrame" ];
+		window.cancelAnimationFrame = window[ vendors[ x ] + "CancelAnimationFrame" ] ||
+			window[ vendors[ x ] + "CancelRequestAnimationFrame" ];
 	}
-
-	if( ! window.requestAnimationFrame && ! window.cancelAnimationFrame ) {
-
-		requestAnimationFrame = function( callback ) {
+	
+	if ( ! window.requestAnimationFrame ) {
+		window.requestAnimationFrame = function( callback, element ) {
 			var currTime = new Date().getTime(),
 				timeToCall = Math.max( 0, 16 - ( currTime - lastTime ) ),
-				id = window.setTimeout(function() {
-						callback( currTime + timeToCall );
-					}, timeToCall );
-
+				id = window.setTimeout( function() {
+					callback( currTime + timeToCall ); 
+				}, timeToCall );
+			
 			lastTime = currTime + timeToCall;
+			
 			return id;
 		};
-
-		cancelAnimationFrame = function(id) {
-			clearTimeout( id );
+	}
+	
+	if ( ! window.cancelAnimationFrame ) {
+		window.cancelAnimationFrame = function( id ) {
+			clearTimeout(id);
 		};
-
 	}
 
 	$.support.transform = "transform" in style
